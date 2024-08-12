3242601624210791263f0ce39087d80fe31f8046
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-12 16:08:40 +0000

    added inputs to voter file

--- routes/voters-route.js
+++ routes/voters-route.js
@@ -4,9 +4,9 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./elections.db");
 
 router.get("/", (req, res) => {
-  let data = {}
+  let data = {};
   // Query the database to find a matching
-  db.all("SELECT * FROM users WHERE role_id=?",[3], function (err, users) {
+  db.all("SELECT * FROM users WHERE role_id=?", [3], function (err, users) {
     data.voters = users;
     if (users.length !== 0) {
       res.render("voters.ejs", { data });
@@ -17,32 +17,58 @@ router.get("/", (req, res) => {
 });
 
 router.get("/registration", (req, res) => {
+  let data = {};
+  let errors;
   // Query the database to find a matching
-  db.all("SELECT * FROM roles", function (err, row) {
-    if (row) {
-      res.render("voter-registration.ejs", { row });
-    } else {
-      console.error(err);
+  db.all("SELECT * FROM roles", function query(err, roles) {
+    data.roles = roles;
+    if (err) errors = err;
+  });
+  db.all("SELECT * FROM positions", function query(err, positions) {
+    data.positions = positions;
+    console.log(data);
+    res.render("voter-registration.ejs", { data });
+    if (err) errors = err;
+    if (errors) {
+      console.error(errors);
     }
   });
 });
 
 // Handle POST request to /voter-registration endpoint
 router.post("/registration", (req, res) => {
-        // Destructure required fields from request body
-        const {first_name, middle_name, last_name, DOB, username, password, photo, role} = req.body;
-        // Insert user information into 'users' table in the database
-        db.run("INSERT INTO users VALUES(?,?,?,?,?,?,?)", [null, first_name, middle_name, last_name, DOB,photo,role], function(err) { 
-            if(err) { 
-                // If there's an error, throw it
-                console.error(err)
-            } else { 
-                // Insert auth information into 'auth' table using last inserted ID
-                db.run("INSERT INTO auth VALUES(?,?,?,?)", [null, username, password, this.lastID]);
-                // Redirect to login page after successful registration
-                res.redirect("/login");
-            }
-        });
+  // Destructure required fields from request body
+  const {
+    first_name,
+    middle_name,
+    last_name,
+    DOB,
+    username,
+    password,
+    photo,
+    role,
+  } = req.body;
+  // Insert user information into 'users' table in the database
+  db.run(
+    "INSERT INTO users VALUES(?,?,?,?,?,?,?)",
+    [null, first_name, middle_name, last_name, DOB, photo, role],
+    function (err) {
+      if (err) {
+        // If there's an error, throw it
+        console.error(err);
+      } else {
+        // Insert auth information into 'auth' table using last inserted ID
+        db.run("INSERT INTO auth VALUES(?,?,?,?)", [
+          null,
+          username,
+          password,
+          this.lastID,
+        ]);
+        // Redirect to login page after successful registration
+        res.redirect("/login");
+      }
+    }
+  );
 });
 
 module.exports = router;
