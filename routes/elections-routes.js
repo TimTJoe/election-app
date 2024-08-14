c9e1715537df37debf78b3e254a4f55c423282f6
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-14 08:30:02 +0000

    added election components

--- routes/elections-routes.js
+++ routes/elections-routes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../utils/db");
 
 router.get("/", function handler(req, res) {
-  res.render("elections.ejs");
+  let data = {};
+  db.all("SELECT * FROM parties", function query(error, parties) {
+    if (error) console.error(error);
+    data.parties = parties;
+    if (data.length !== 0) {
+      res.render("elections.ejs", { data });
+    }
+  });
 });
 router.get("/create", function handler(req, res) {
   res.send("create elections");
--- routes/index-route.js
+++ routes/index-route.js
@@ -8,43 +8,50 @@ router.get("/welcome", function get(req, res) {
 });
 
 router.get("/", function get(req, res) {
-  let data = {}
-  db.all("SELECT * FROM users WHERE role_id=?", [3], function query(error, voter) {
-    if(error) console.error(error)
-    data.voter = voter
-  });
+  let data = {};
+  db.all(
+    "SELECT * FROM users WHERE role_id=?",
+    [3],
+    function query(error, voter) {
+      if (error) console.error(error);
+      data.voter = voter;
+    }
+  );
 
-  db.all("SELECT * FROM users WHERE role_id=?", [3], function query(error, voter) {
-    if(error) console.error(error)
-    data.voter = voter
+  db.all("SELECT * FROM parties", function query(error, parties) {
+    if (error) console.error(error);
+    data.parties = parties;
+    if (data.length !== 0) {
+      res.render("dashboard.ejs", { data });
+    }
   });
 });
 
 // Handle POST request to /login endpoint
 router.post("/login", (req, res) => {
   let data = {};
-    // Extract username and password from request body
-    const { username, password } = req.body;
+  // Extract username and password from request body
+  const { username, password } = req.body;
 
-    // Query the database to find a matching username and password
-    db.all(
-      "SELECT * from auth WHERE username=? AND password=?",
-      [username, password],
-      function query(error, user) {
-        if (user?.length == 0) {
-          data.error = "Incorrect username or password."
-          res.render("login.ejs", { data });
-        } else {
-          data.user = user;
-          res.render("dashboard.ejs", { data });
-        }
+  // Query the database to find a matching username and password
+  db.all(
+    "SELECT * from auth WHERE username=? AND password=?",
+    [username, password],
+    function query(error, user) {
+      if (user?.length == 0) {
+        data.error = "Incorrect username or password.";
+        res.render("login.ejs", { data });
+      } else {
+        data.user = user;
+        res.render("dashboard.ejs", { data });
       }
-    );
+    }
+  );
 });
 
 router.get("/login", (req, res) => {
-  let data = {}
-  res.render("login.ejs", {data});
+  let data = {};
+  res.render("login.ejs", { data });
 });
 
 module.exports = router;
