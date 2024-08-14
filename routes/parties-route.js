1f6332e535ae77435934d69fe322ab1219ddf566
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-14 15:03:14 +0000

    added entities files

--- routes/parties-route.js
+++ routes/parties-route.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const db = require("../utils/db");
-const upload = require("../utils/upload");
 const router = express.Router();
+const db = require("../utils/db");
+const upload = require("../public/js/multer");
 
 router.get("/", (req, res) => {
   let data = {};
@@ -19,25 +19,24 @@ router.get("/registration", (req, res) => {
 
 // Handle POST requests to "/party-registration"
 router.post("/registration", upload.single("logo"), (req, res) => {
-  let filename = req.new_file_name;
-  console.log(filename);
+  let logo = req.file.filename
   // Destructure party and logo from the request body
-  // const { party, logo } = req.body;
+  const { party } = req.body;
 
-  // // Insert party details into the database
-  // db.run(
-  //   "INSERT INTO parties VALUES (?,?,?)",
-  //   [null, party, logo],
-  //   function query(err) {
-  //     if (!err) {
-  //       // Redirect to the dashboard if insertion is successful
-  //       res.redirect("/parties");
-  //     } else {
-  //       // console log if insertion fails
-  //       console.error(err);
-  //     }
-  //   }
-  // );
+  // Insert party details into the database
+  db.run(
+    "INSERT INTO parties VALUES (?,?,?)",
+    [null, party, logo],
+    function query(err) {
+      if (!err) {
+        // Redirect to the dashboard if insertion is successful
+        res.redirect("/parties");
+      } else {
+        // console log if insertion fails
+        console.error(err);
+      }
+    }
+  );
 });
 
 module.exports = router;
