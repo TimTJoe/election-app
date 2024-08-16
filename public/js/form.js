15b59899c9ce0cd00dffe958c8af6011c9f82c91
timtjoe <ttjoe0@gmail.com>
2024-08-16 15:21:20 +0000

    add: hide or show party selection file during registration

--- public/js/form.js
+++ public/js/form.js
@@ -4,6 +4,11 @@ let spanID = document.getElementById("spanID");
 window.onload = function onload() {
   spanID.classList.add("hide");
   role.addEventListener("change", function handler(evt) {
-      console.log(evt);
+    let choice = Number(role.options[role.selectedIndex].value);
+    if (choice === 2) {
+      spanID.classList.remove("hide");
+    } else {
+      spanID.classList.add("hide");
+    }
   });
 };
