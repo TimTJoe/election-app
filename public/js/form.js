1b5d0cbe170aba7ed149d3230892c58ac29d97e9
timtjoe <ttjoe0@gmail.com>
2024-08-16 15:22:46 +0000

    added js doc comments

--- public/js/form.js
+++ public/js/form.js
@@ -3,6 +3,15 @@ let spanID = document.getElementById("spanID");
 
 window.onload = function onload() {
   spanID.classList.add("hide");
+  
+  /**
+   * Handles the change event for the 'role' dropdown menu.
+   * When the selected option changes, this function checks the value of the selected option.
+   * If the value is 2, it removes the 'hide' class from the spanID element to make it visible.
+   * Otherwise, it adds the 'hide' class to the spanID element to hide it.
+   *
+   * @param {Event} evt - The event object representing the change event.
+   */
   role.addEventListener("change", function handler(evt) {
     let choice = Number(role.options[role.selectedIndex].value);
     if (choice === 2) {
