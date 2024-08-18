f638248a8d88a4530efd22459b3a8618999103bf
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-18 07:45:13 +0000

    added elections parge

--- public/js/elections.js
+++ public/js/elections.js
@@ -1,22 +1,33 @@
 var electionSelect = document.getElementById("elections-select");
 var electionTabs = document.getElementById("elections-tabs");
+var electionName = document.getElementById("election-name");
 
 window.onload = function onload() {
-  document.querySelectorAll(".tabitem").forEach((items) => {
-    items.classList.remove("active");
-  });
-
   electionSelect.addEventListener("change", function handler(evt) {
-    let choice = Number(
-      electionSelect.options[electionSelect.selectedIndex].value
-    );
-    console.log(choice);
+    let choice = electionSelect.options[electionSelect.selectedIndex].innerText;
+    electionName.innerText = choice;
   });
 
-  electionTabs.addEventListener("click", function tabs(evt) {
-    document.querySelectorAll(".tabitem").forEach((items) => {
-      items.classList.remove("active");
+  document.querySelectorAll(".tab").forEach(function each(tab) {
+    tab.addEventListener("click", function click(evt) {
+      removeClass("tab", "active");
+      evt.target.classList.add("active");
     });
-    evt.target.classList.add("active");
   });
 };
+
+function removeClass(targetElem, classToRemove) {
+  document.querySelectorAll("." + targetElem).forEach(function remove(item) {
+    item.classList.remove(classToRemove);
+  });
+}
+
+// document.querySelectorAll(".tabitem").forEach((items) => {
+//   items.classList.remove("active");
+// });
+// electionTabs.addEventListener("click", function tabs(evt) {
+//   document.querySelectorAll(".tabitem").forEach((items) => {
+//     items.classList.remove("active");
+//   });
+//   evt.target.classList.add("active");
+// });
