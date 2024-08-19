44282c1fff6df0a9a3fa4d96611a718fc624e7d6
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-19 09:54:09 +0000

    added election details view

--- routes/elections-routes.js
+++ routes/elections-routes.js
@@ -8,31 +8,37 @@ router.get("/", function handler(req, res) {
     if (error) console.error(error);
     data.parties = parties;
     if (data.length !== 0) {
-      res.render("elections.ejs", {path: "elections", data });
+      res.render("elections.ejs", { path: "elections", data });
     }
   });
 });
 router.get("/create", function handler(req, res) {
   res.send("create elections");
 });
-
-router.get("/:electionID/parties", (req, res) => {
+router.get("/:electionID", function handler(req, res) {
   let data = {};
-  db.all("SELECT * FROM parties", function (err, parties) {
-    data.parties = parties;
-    if (err) console.error(err);
-    res.render("elections/parties.ejs", { data });
-  });
+  let { electionID } = req.params;
+  data.electionID = electionID;
+  res.render("election-details.ejs", { path: "elections", data });
 });
 
-router.get("/:electionID/candidates", (req, res) => {
-  let data = {};
-  res.render("elections/candidates.ejs", { data });
-});
+// router.get("/:electionID/parties", (req, res) => {
+//   let data = {};
+//   db.all("SELECT * FROM parties", function (err, parties) {
+//     data.parties = parties;
+//     if (err) console.error(err);
+//     res.render("elections/parties.ejs", { data });
+//   });
+// });
 
-router.get("/:electionID/voters", (req, res) => {
-  let data = {};
-  res.render("elections/voters.ejs", { data });
-});
+// router.get("/:electionID/candidates", (req, res) => {
+//   let data = {};
+//   res.render("elections/candidates.ejs", { data });
+// });
+
+// router.get("/:electionID/voters", (req, res) => {
+//   let data = {};
+//   res.render("elections/voters.ejs", { data });
+// });
 
 module.exports = router;
