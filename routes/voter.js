a049d5ab1245c884a491665262b44782c7ecbc34
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-22 17:50:21 +0000

    11.Display the roles in the registration form when creating accounts

--- routes/voter.js
+++ routes/voter.js
@@ -2,12 +2,22 @@ var router = require("express").Router();
 var db = require("../db");
 
 router.get("/registration", (req, res, next) => {
+  let data = {};
   db.all("SELECT * FROM roles", (err, rows) => {
     if (!err) {
-      console.log(rows);
+      data.roles = rows;
+      res.render("voter-registration.ejs", {
+        title: "Voter Registration",
+        data,
+      });
+    } else {
+      console.error(err);
     }
   });
-  res.render("voter-registration.ejs", { title: "Voter Registration" });
 });
+
+router.post("/registration", (req,res)=>{
+    console.log(req.body)
+})
 
 module.exports = router;
