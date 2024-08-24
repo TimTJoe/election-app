82cdd99b0921216d1cd875ddcef9e42382b1e75f
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-24 10:03:51 +0000

    display candidates

--- routes/vote.js
+++ routes/vote.js
@@ -7,9 +7,9 @@ router.get("/", (req, res, next) => {
   // SELECT * FROM candidates INNER JOIN parties ON parties.id=candidates.party_id
   // SELECT * FROM candidates JOIN parties ON parties.id=candidates.party_id
   db.all(
-    "SELECT * FROM candidates INNER JOIN parties ON parties.id=candidates.party_id",
+    "SELECT *, candidates.id FROM candidates LEFT OUTER JOIN parties ON parties.id=candidates.party_id LEFT OUTER JOIN positions ON positions.id = candidates.position_id",
     function (err, rows) {
-      console.log(rows);
+      //TODO: add each candidate to it position property on data ie: data.president, data.vp
       if (err) console.error(err);
       data.candidates = rows;
       res.render("vote.ejs", { title: "Cast Your Vote", data });
@@ -18,23 +18,29 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-  let { president, voter } = req.body;
+  let { candidate, voter } = req.body;
   //TODO: update user table
   db.run(
     "INSERT INTO votes VALUES (?,?,?,?)",
-    [null, president, 1, voter],
+    [null, candidate, true, voter],
     function (err) {
-      if (err) console.error(err);
-      res.redirect("/");
+      if (err) {
+        console.error(err);
+      } else {
+        db.run(
+          "UPDATE users SET voted=? WHERE id=?",
+          [true, voter],
+          function (err) {
+            if (err) {
+              console.error(err);
+            } else {
+              res.redirect("/");
+            }
+          }
+        );
+      }
     }
   );
-  db.run("UPDATE users SET voted=? WHERE id=?", [true, voter], function (err) {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(`Row(s) updated: ${this.changes}`);
-    }
-  });
 });
 
 module.exports = router;
