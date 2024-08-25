5dbded9976bc0cf48b1f13673dd6ef780ea56a4e
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-25 12:20:08 +0000

    designed select to vote details

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -15,11 +15,15 @@ router.get("/", (req, res, next) => {
   db.all("SELECT COUNT(*) AS total FROM votes", function query(error, total) {
     error ? console.error(error) : (data.total.votes = total[0].total);
   });
+  db.all("SELECT COUNT(*) AS total FROM candidates", function query(error, total) {
+    error ? console.error(error) : (data.total.candidates = total[0].total);
+  });
     db.all(
       //TODO: add all the
       "SELECT *,candidates.id, (SELECT SUM(votes.vote) FROM votes WHERE candidates.id=votes.candidate_id) votes FROM candidates LEFT OUTER JOIN parties ON parties.id=candidates.party_id LEFT OUTER JOIN positions ON positions.id = candidates.position_id",
       function (err, rows) {
-        err ? console.error(err) : (data.votes = rows);
+        console.log(rows)
+        err ? console.error(err) : (data.candidates = rows);
       }
     );
   db.all("SELECT * FROM users WHERE role_id=?", [3], function (error, rows) {
